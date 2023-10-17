9eba61f88c8119e549e01b233831fdcf0d0194de
slvler <56368718+slvler@users.noreply.github.com>
2023-10-18 02:38:00 +0300

    Add files via upload

---
+++ dummy_image.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+NC=$'\e[0m'
+
+# Background
+REDBg=$'\e[0;41m'
+GREENBg=$'\e[0;42m'
+YELLOWBg=$'\e[0;43m'
+BLUEBg=$'\e[0;44m'
+PINKBg=$'\e[0;45m'
+CYANBg=$'\e[0;46m'
+WHITEBg=$'\e[0;47m'
+
+
+# Color
+RED=$'\e[0;31m'
+GREEN=$'\e[0;32m'
+YELLOW=$'\e[0;33m'
+BLUE=$'\e[0;34m'
+PINK=$'\e[0;35m'
+CYAN=$'\e[0;36m'
+WHITE=$'\e[0;37m'
+
+
+# Underline
+URED=$'\e[4;31m'
+UGREEN=$'\e[4;32m'
+UYELLOW=$'\e[4;33m'
+UBLUE=$'\e[4;34m'
+UPINK=$'\e[4;35m'
+UCYAN=$'\e[4;36m'
+UWHITE=$'\e[4;37m'
+
+
+
+echo "${YELLOW} Dummy image: ${NC}"
